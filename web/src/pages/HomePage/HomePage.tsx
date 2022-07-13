cb7698dfccdf8039ee1896053e48b1353eb95580
Amine <amineprojet7@gmail.com>
2022-07-13 10:08:38 +0200

    fix: few things

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -8,7 +8,7 @@ const HomePage = () => {
       <img
         src={'https://i.kym-cdn.com/photos/images/newsfeed/001/042/619/4ea.jpg'}
         alt="John Travolta"
-        className="my-auto mx-auto pt-5 rounded"
+        className="my-auto mx-auto rounded-xl mt-20"
       />
     </>
   )
